95e2ff8b65a84074a3a057d101675f93f56f80b1
Merrick Fox <merrick.fox@gmail.com>
2016-09-03 00:48:01 +0100

    upgrade complete...

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import "./polyfills.ts";
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule, environment } from './app/';
+
 
 if (environment.production) {
   enableProdMode();
